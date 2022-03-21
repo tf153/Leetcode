ec9e3879f16c085b076f994d8d92abc81636ef32
Rahul Joshi <43797501+tf153@users.noreply.github.com>
2022-03-21 07:57:29 +0530

    todays

---
+++ Questions/1_Two_sum.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        vector<int> ans(2);
+        int i = 0, j = nums.size() - 1;
+        vector<int> temp = nums;
+        sort(nums.begin(), nums.end());
+        while (i < j)
+        {
+            int sum = nums[i] + nums[j];
+            if (sum > target)
+            {
+                j--;
+            }
+            else if (sum < target)
+            {
+                i++;
+            }
+            else
+            {
+                for (int l = 0; l < nums.size(); l++)
+                {
+                    if (temp[l] == nums[i])
+                    {
+                        ans[0] = l;
+                        nums[i] = -1;
+                    }
+                    else if (temp[l] == nums[j])
+                    {
+                        ans[1] = l;
+                        nums[j] = -1;
+                    }
+                }
+                return ans;
+            }
+        }
+
+        return ans;
+    }
+};
