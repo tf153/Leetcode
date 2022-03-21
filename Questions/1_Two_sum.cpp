55f2d5521af99d504225dcbb6f1658f317f1d106
Rahul Joshi <43797501+tf153@users.noreply.github.com>
2022-03-21 07:59:17 +0530

    changes

--- Questions/1_Two_sum.cpp
+++ Questions/1_Two_sum.cpp
@@ -3,40 +3,12 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
-        vector<int> ans(2);
-        int i = 0, j = nums.size() - 1;
-        vector<int> temp = nums;
-        sort(nums.begin(), nums.end());
-        while (i < j)
-        {
-            int sum = nums[i] + nums[j];
-            if (sum > target)
-            {
-                j--;
-            }
-            else if (sum < target)
-            {
-                i++;
-            }
-            else
-            {
-                for (int l = 0; l < nums.size(); l++)
-                {
-                    if (temp[l] == nums[i])
-                    {
-                        ans[0] = l;
-                        nums[i] = -1;
-                    }
-                    else if (temp[l] == nums[j])
-                    {
-                        ans[1] = l;
-                        nums[j] = -1;
-                    }
-                }
-                return ans;
-            }
+        unordered_map<int, int> indices;
+        for (int i = 0; i < nums.size(); i++) {
+            if (indices.find(target - nums[i]) != indices.end())
+                return {indices[target - nums[i]], i};
+            indices[nums[i]] = i;
         }
-
-        return ans;
+        return {};
     }
 };
