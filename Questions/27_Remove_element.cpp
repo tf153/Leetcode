7da932de24e624894be2c3a4aa8b4d3e01090c71
Rahul Joshi <43797501+tf153@users.noreply.github.com>
2022-04-07 10:09:28 +0530

    one more

---
+++ Questions/27_Remove_element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n=nums.size();
+        int i=0,j=0;
+        while(j!=n){
+            if(nums[j]==val)
+                j++;
+            else
+                nums[i++]=nums[j++];
+        }
+        return i;
+    }
+};
